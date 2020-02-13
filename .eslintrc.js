fc287b6e3286010df436d5c1e02a6ea9f202c8d5
woody <wdwantu@yahoo.com>
2020-02-13 15:07:24 +0800

    Create .eslintrc.js
    更新eslint配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+  node: true
+  },
+  extends: [
+  "plugin:vue/essential"
+  // 'standard'
+  ],
+  rules: {
+  "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+  "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  plugins: ["vue", "html"],
+  parserOptions: {
+  parser: "babel-eslint"
+  }
+  };
+  
+  
+  
+  
+  
